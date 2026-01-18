ddb2c7414b00c97d4b6154e5b93beff2c590c118
MaruchanPicosa <zzzaida09@gmail.com>
2026-01-17 23:10:27 -0600

    Inicio

--- app.py
+++ app.py
@@ -15,7 +15,7 @@ db = client["HomiDB"]
 usuarios = db["usuarios"]
 mongo = db
 
-# Página principal - LOGIN
+# Página principal - LOGIiN
 @app.route("/")
 def home():
     propiedades = consultas.obtener_propiedades_destacadas(mongo.db)
